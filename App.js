86a0ee86a532837594575ac32c99621fedd3cf9b
Thomasosho <tomasosho@gmail.com>
2022-09-16 01:24:55 +0100

    cli

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screen/HomeScreen';
+// import {TailwindProvider} from 'tailwind-rn';
+// import utilities from './tailwind.json';
 import { TailwindProvider } from 'tailwindcss-react-native';
+import GlobalStyles from './GlobalStyles';
+import { SafeAreaView } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,9 +14,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Index" component={HomeScreen} />
-        </Stack.Navigator>
+        <SafeAreaView style={GlobalStyles.droidSafeArea}>
+          <Stack.Navigator>
+              <Stack.Screen name="Index" component={HomeScreen} />
+          </Stack.Navigator>
+        </SafeAreaView>
       </TailwindProvider>
     </NavigationContainer>
   );
--- screen/HomeScreen.jsx
+++ screen/HomeScreen.jsx
@@ -1,12 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar';
+import { View, Text, Image, TextInput, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { 
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon, 
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
+  
+  const navigation = useNavigation();
+
+  useLayoutEffect(()  => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-center text-red-600">Osho is my name</Text>
-      <StatusBar style="auto" />
+    <View className="bg-white pt-5">
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+          <Image 
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }}
+            // style={{width: 30, height: 30}}
+            className="h-7 w-7 bg-slate-300 p-4 rounded-full"
+          />
+
+          <View className="flex-1">
+            <Text className="font-bold text-grey-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#7ab259" />
+            </Text>
+          </View>
+          <UserIcon size={35} color="#7ab259"/>
+        </View>
+
+        {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row flex-1 space-x-2 bg-slate-200 p-3">
+            <MagnifyingGlassIcon color="gray" size={20}/>
+            <TextInput
+              placeholder='Search Tinkoko Products'
+              keyboardType='default' />
+          </View>
+
+          <AdjustmentsVerticalIcon color="#7ab259" />
+        </View>
+      </Text>
     </View>
   )
 }
